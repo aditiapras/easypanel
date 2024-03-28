1eadc73fa6ca81d1e368f4020bad9c76ff85d4ab
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-29 03:08:41 +0700

    Update route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello World" });
+  const users = await prisma.user.findMany();
+  return NextResponse.json({ message: "Hello World", users });
 }
