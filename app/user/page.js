1a26a98fd45f8cf2ad95b4a18fab5b58cc417722
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-29 02:26:38 +0700

    add api and new page

---
+++ app/user/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-screen">
+      User Page
+    </div>
+  );
+}
