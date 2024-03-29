f67b98fa4ce623e616eb5ad30ff15ad58a8ba40c
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-30 01:13:13 +0700

    Update page.js

--- app/user/page.js
+++ app/user/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
-      User Page
+      User Page Testing 1
     </div>
   );
 }
