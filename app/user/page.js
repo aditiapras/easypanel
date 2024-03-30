6cd3bf3564fb226ac7879ca52d2005e09205169d
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-31 00:34:28 +0700

    Update page.js

--- app/user/page.js
+++ app/user/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
-      User Page Testing 1 with token using heroku
+      Deployment on Easypanel using Heroku
     </div>
   );
 }
