6ac5c415834e896b44621e96d537e86ca0b99aaa
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-31 00:13:04 +0700

    Update page.js

--- app/user/page.js
+++ app/user/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
-      User Page Testing 1 with token
+      User Page Testing 1 with token using heroku
     </div>
   );
 }
