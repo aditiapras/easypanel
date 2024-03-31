a85840454b05fe8cf49c8eed9fdb628ac67b944c
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-31 21:23:08 +0700

    testing session

--- app/user/page.js
+++ app/user/page.js
@@ -1,20 +1,39 @@
-import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
-import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen p-5">
       <div className="mb-5 flex gap-5">
-        <Skeleton className="w-20 h-20 rounded-full" />
+        <Avatar>
+          <AvatarImage src={session?.user?.image} alt={session?.user?.name} />
+          <AvatarFallback>AP</AvatarFallback>
+        </Avatar>
         <div className="flex flex-col gap-2">
-          <Skeleton className="w-52 h-5 rounded-full" />
           <Skeleton className="w-52 h-5 rounded-full" />
           <Skeleton className="w-52 h-5 rounded-full" />
         </div>
       </div>
-      <Button>Submit</Button>
-      <p className="text-2xl mt-5">Deployment on Easypanel using Heroku</p>
+      <p className="mb-5 text-xs">
+        Currently logged in user:{" "}
+        <span className="font-medium">{session?.user?.name}</span>{" "}
+        <span className="text-zinc-400">({session?.user?.id})</span>
+      </p>
+      <p className="text-xs mb-5 text-zinc-500">
+        Deployment on{" "}
+        <a
+          href="https://easypanel.io"
+          target="_blank"
+          className="hover:underline text-zinc-900"
+        >
+          easypanel.io
+        </a>{" "}
+        using <span className="font-semibold text-zinc-900">Heroku Build</span>
+      </p>
     </div>
   );
 }
