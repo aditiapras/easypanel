bf300dc238cc68bfdefc8ab47d4b475eabf5bc6e
Aditia Prasetian <aditiapras97@gmail.com>
2024-03-31 20:52:59 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,21 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard"] };
+export const config = { matcher: ["/dashboard", "/api/users"] };
+
+export async function middleware(req) {
+  const token = await getToken({ req });
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/api/users")) {
+    if (!token) {
+      return NextResponse.json({ error: "Not Authorized", status: 401 });
+    }
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+}
