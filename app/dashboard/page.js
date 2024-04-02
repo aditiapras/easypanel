be890f6f51c3e157b74f38ba12e011d4d8c56aeb
Aditia Prasetian <aditiapras97@gmail.com>
2024-04-03 00:24:26 +0700

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,7 +3,7 @@ import SignOut from "./signout";
 export default function Page() {
   return (
     <main className="w-full min-h-screen flex flex-col bg-zinc-100 items-center justify-center px-5 md:px-0">
-      Dashboard
+      <p className="mb-5">Dashboard</p>
       <SignOut />
     </main>
   );
