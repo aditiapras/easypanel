ab6fbb6d6762ae575fee63c393aa7499a266fecc
Aditia Prasetian <aditiapras97@gmail.com>
2024-04-03 00:24:29 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-900">
+      <p className="text-xl font-light mt-5 text-white">PT Citra Mekar Jaya</p>
+    </main>
   );
 }
